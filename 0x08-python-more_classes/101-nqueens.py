e3f896a01172a9d25f5f580da76e1139945c9532
Nwachiemu <emuchaynwachi@gmail.com>
2023-11-29 13:36:54 -0800

    added

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle.
+"""Solvess the N-queens puzzle.
 
-Determines all possible solutions to placing N
-N non-attacking queens on an NxN chessboard.
+Determiness all possible solutions to placing N
+N non-attaccking queens on an NxN chessboard.
 
 Example:
     $ ./101-nqueens.py N
@@ -14,14 +14,14 @@ Attributes:
     solutions (list): A list of lists containing solutions.
 
 Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
-where `r` and `c` represent the row and column, respectively, where a
+where r and c represent the row and column, respectively, where a
 queen must be placed on the chessboard.
 """
 import sys
 
 
 def init_board(n):
-    """Initialize an `n`x`n` sized chessboard with 0's."""
+    """Initialize an n`x`n sized chessboard with 0's."""
     board = []
     [board.append([]) for i in range(n)]
     [row.append(' ') for i in range(n) for row in board]
@@ -125,7 +125,7 @@ def recursive_solve(board, row, queens, solutions):
     return (solutions)
 
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
